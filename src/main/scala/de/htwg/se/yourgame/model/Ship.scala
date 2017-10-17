623e94fdc727f47bcdc760dadba9202924529ccb
Lukas Hansen <luhansen@htwg-konstanz.de>
2017-10-17 15:19:08 +0200

    model

---
+++ src/main/scala/de/htwg/se/yourgame/model/Ship.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.yourgame.model
+
+case class Ship(size: Int, COLOR: Enumeration) {
+
+}
--- src/main/scala/de/htwg/se/yourgame/model/Student.scala
+++ src/main/scala/de/htwg/se/yourgame/model/Student.scala
@@ -3,4 +3,3 @@ package de.htwg.se.yourgame.model
 case class Student(name: String) {
   def f(x: Int): Int = x + 1
 }
-
