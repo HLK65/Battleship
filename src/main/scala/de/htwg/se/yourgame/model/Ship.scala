9b596681ea5374738b076f0ca4192dff372ec5cb
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-10-20 09:32:17 +0200

    Added attributes to ship class
    improved construktor added attributes added method to lose hitpoints

--- src/main/scala/de/htwg/se/yourgame/model/Ship.scala
+++ src/main/scala/de/htwg/se/yourgame/model/Ship.scala
@@ -1,5 +1,21 @@
 package de.htwg.se.yourgame.model
 
-case class Ship(size: Int, COLOR: Enumeration) {
+/**
+  * Creates the Ship. The size and color can´t be changed.
+  * @param size the amount of fields the ship takes.
+  * @param COLOR the color the ships has. THe player has the same color so that ships can be linked to the player
+  */
+case class Ship(val size: Int, val COLOR: Enumeration) {
 
+  var hitPoints = size
+
+  /**
+    * Removes one point from the hitpoints
+    * @return new hitpoints
+    */
+  def hitShip(): Int = {
+    hitPoints = hitPoints-1
+    return hitPoints
+  }
 }
+
