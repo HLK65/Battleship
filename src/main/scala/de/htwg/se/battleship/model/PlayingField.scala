0f664ed24b6d376c31cc069ae395ea775a7cdb38
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-10-23 11:16:59 +0200

    Changed PlayingField to use Points

--- src/main/scala/de/htwg/se/battleship/model/PlayingField.scala
+++ src/main/scala/de/htwg/se/battleship/model/PlayingField.scala
@@ -2,15 +2,15 @@ package de.htwg.se.battleship.model
 
 case class PlayingField(size: Int,playerOne: Player, playerTwo: Player) {
 
-  val grid = Array.ofDim[Ship](size, size)
+  val grid = Set[Point] = Set()
 
-  def hitField(x: Int, y: Int): Boolean = {
+  def hitField(point: Point): Boolean = {
       //check if there is an enemy ship on this field
       //if true hit ship remove one point return true
       //if false return false
   }
   def placeShip(playerColor: String, point: Point, size: Int) = {
-      
+    
   }
 
 }
