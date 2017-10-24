5e3c40bc8ee6138a5715ad822b300b08bead0855
Lukas Hansen <luhansen@htwg-konstanz.de>
2017-10-24 13:41:17 +0200

    tui update

--- src/main/scala/de/htwg/se/battleship/view/TuiView.scala
+++ src/main/scala/de/htwg/se/battleship/view/TuiView.scala
@@ -6,14 +6,23 @@ import scala.util.Random
 
 class TuiView {
   def printField(field: Field): Unit = {
-    for (y <- 1 to field.size) {
+    for (y <- 0 to field.size) {
       println()
-      for (x <- 1 to field.size) {
-        if (Random.nextBoolean()) {
-          print("x ")
+      for (x <- 0 to field.size) {
+        if (x == 0 && y == 0) print("   ") //0 0
+        else if (y == 0 && x != 0) {
+          if (x.toString.length == 2) print(" " + x)
+          else print(" " + x + " ")
+        }
+        else if (x == 0 && y != 0) {
+          if (y.toString.length == 2) print(" " + y)
+          else print(" " + y + " ")
+        }
+        else if (Random.nextBoolean()) {
+          print(" x ")
         }
         else {
-          print("- ")
+          print(" - ")
         }
       }
     }
