2cda5ddaa320240f8cc8b5d0d282c65597952166
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-10-24 14:15:28 +0200

    removed SHip array from point
    it will now be uesed in the field

--- src/main/scala/de/htwg/se/battleship/model/Point.scala
+++ src/main/scala/de/htwg/se/battleship/model/Point.scala
@@ -1,20 +1,4 @@
 package de.htwg.se.battleship.model
 
-case class Point(x: Int, y: Int) {
-  var ships = new Array[Ship](2) //TODO var vermeiden
-  def placeShip(ship: Ship) = {
-    if (ships(0) == null) {
-      ships(0) = ship
-    } else {
-      ships(1) = ship
-    }
-  }
-  def hitPoint(): Boolean = {
-    if (ships.isEmpty) {
-      return false
-    } else {
-      ships = new Array[Ship](2)
-      return true
-    }
-  }
+case class Point(x: Int, y: Int, empty:Boolean) {
 }
