f4a47cd1fc70963af88fe15fb4f9881dc03ecc50
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-11-14 13:42:52 +0100

    fixed ship place bug

--- src/main/scala/de/htwg/se/battleship/controller/Controller.scala
+++ src/main/scala/de/htwg/se/battleship/controller/Controller.scala
@@ -1,22 +1,20 @@
 package de.htwg.se.battleship.controller
 
-import de.htwg.se.battleship.model.{Field, Orientation, Player, Point}
+import de.htwg.se.battleship.model.{ Field, Orientation, Player, Point }
 
 case class Controller(fieldSize: Int) {
 
   val player1Color = "Red"
   val player2Color = "Blue"
 
-
   val field1 = Field(fieldSize)
   val field2 = Field(fieldSize)
 
   //1x5Felder, 2x4Felder, 3x3Felder, 4x2Felder
-  val shipConfig: scala.collection.mutable.Map[Int, Int] = scala.collection.mutable.Map(5 -> 1, 4 -> 2 /*, 3 -> 3, 2 -> 4*/)
+  val shipConfig: scala.collection.mutable.Map[Int, Int] = scala.collection.mutable.Map(5 -> 1, 4 -> 2 /*, 3 -> 3, 2 -> 4*/ )
 
   val player1 = Player(player1Color, field1, shipConfig)
   val player2 = Player(player2Color, field2, shipConfig)
-
 
   def placeShip(player: Player, startPoint: Point, shipSize: Int, orientation: Orientation): Boolean = {
     player.field.placeShip(startPoint, shipSize, orientation.toString)
