51d5783aaf96ebe8a06a4826179fa1396c229529
Lukas Hansen <luhansen@htwg-konstanz.de>
2017-11-21 12:32:28 +0100

    PlayerSpec

---
+++ src/test/scala/de/htwg/se/battleship/model/PlayerSpec.scala
@@ -0,0 +1,24 @@
+package de.htwg.se.battleship.model
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class PlayerSpec extends FlatSpec with Matchers {
+  //TODO check for "java before"
+  val shipConfig: scala.collection.mutable.Map[ /*size*/ Int, /*amount*/ Int] =
+    scala.collection.mutable.Map(/*5 -> 1, 4 -> 2, 3 -> 3, */ 2 -> 1)
+  val playerColor = "Red"
+  val field = Field(2)
+  val player = Player(playerColor, Field(2), shipConfig)
+
+  "A Player" should "have a Color" in {
+    player.COLOR should be(playerColor)
+  }
+
+  "A Player" should "have a Field" in {
+    player.field should be(field)
+  }
+
+  "A Player" should "hava a shipInventory" in {
+    player.shipConfig should be(shipConfig)
+  }
+}
