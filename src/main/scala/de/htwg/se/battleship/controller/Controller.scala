fbe63cbf22951dc2d410f9fd092f004bc47f35bc
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-11-21 11:37:34 +0100

    Tests for ship fixed

--- src/main/scala/de/htwg/se/battleship/controller/Controller.scala
+++ src/main/scala/de/htwg/se/battleship/controller/Controller.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.battleship.controller
 
-import de.htwg.se.battleship.model.{Field, Orientation, Player, Point}
+import de.htwg.se.battleship.model.{ Field, Orientation, Player, Point }
 
 case class Controller(fieldSize: Int) {
 
@@ -11,7 +11,7 @@ case class Controller(fieldSize: Int) {
   val field2 = Field(fieldSize)
 
   //1x5Felder, 2x4Felder, 3x3Felder, 4x2Felder
-  val shipConfig: scala.collection.mutable.Map[ /*size*/ Int, /*amount*/ Int] = scala.collection.mutable.Map(/*5 -> 1, 4 -> 2, 3 -> 3, */ 2 -> 1)
+  val shipConfig: scala.collection.mutable.Map[ /*size*/ Int, /*amount*/ Int] = scala.collection.mutable.Map( /*5 -> 1, 4 -> 2, 3 -> 3, */ 2 -> 1)
 
   val player1 = Player(player1Color, field1, shipConfig.clone())
   val player2 = Player(player2Color, field2, shipConfig.clone())
--- src/test/scala/de/htwg/se/battleship/model/ShipSpec.scala
+++ src/test/scala/de/htwg/se/battleship/model/ShipSpec.scala
@@ -3,19 +3,14 @@ package de.htwg.se.battleship.model
 import org.scalatest._
 
 class ShipSpec extends FlatSpec with Matchers {
-  /* "A Ship" should "have a color" in {
-     Ship(2, "white").COLOR should be("white")
-   }
 
-   "A Ship" should "have a size" in {
-     Ship(2, "white").SIZE should be(2)
-   }
-
-   "A Ship" should "have hitpoints" in {
-     Ship(2, "white").hitPoints should be(2)
-   }
-
-   "A Ship" should "loose hitPoints" in {
-     val ship = Ship(2, "white").hitShip() should be(1)
-   }*/
+  "A Ship" should "have a Size" in {
+    var ship = Ship(5).SIZE should be(5)
+  }
+  "A Ship" should "have hitpoints" in {
+    var ship = Ship(5).hitPoints should be(5)
+  }
+  "A Ship" should "lose hitPoints" in {
+    var ship = Ship(5).hitShip() should be(4)
+  }
 }
