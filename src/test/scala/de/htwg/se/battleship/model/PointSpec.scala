08ae737ba7e7760378e2540c327b4df7b167da7a
SaschanenDatum <sascha.drobig@htwg-konstanz.de>
2017-11-21 11:41:31 +0100

    Point test

---
+++ src/test/scala/de/htwg/se/battleship/model/PointSpec.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.battleship.model
+
+import org.scalatest._
+
+class PointSpec extends FlatSpec with Matchers {
+
+  "A Point" should "have a x value " in {
+    var point = Point(1, 2).x should be(1)
+  }
+  "A Point" should "have a y value" in {
+    var point = Point(1, 2).y should be(2)
+  }
+}
