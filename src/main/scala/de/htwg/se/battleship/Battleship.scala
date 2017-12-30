a2dce78e3e0b867d2d60b5f560867f659a315b9b
Lukas Hansen <luhansen@htwg-konstanz.de>
2017-12-30 18:09:04 +0100

    configurable size

--- src/main/scala/de/htwg/se/battleship/Battleship.scala
+++ src/main/scala/de/htwg/se/battleship/Battleship.scala
@@ -2,14 +2,19 @@ package de.htwg.se.battleship
 
 import de.htwg.se.battleship.controller.Controller
 import de.htwg.se.battleship.model._
-import de.htwg.se.battleship.view.{ TuiView }
+import de.htwg.se.battleship.view._
 
 object Battleship {
+
+  val fieldSizeDefault = 20
+  val fieldSizeEnvName = "fieldSize"
+
   def main(args: Array[String]): Unit = {
 
-    //TODO make size changeable
-    val controller = Controller(20, new TuiView())
-    controller.gameStart()
+    val fieldSize: Int = if (sys.env(fieldSizeEnvName).isEmpty) fieldSizeDefault else sys.env(fieldSizeEnvName).toInt
+
+    val controller = Controller(fieldSize, new TuiView)
+        controller.gameStart()
 
   }
 }
