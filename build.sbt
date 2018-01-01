1e8bc386a8d4a9228268e8ef712367121bf52412
Lukas Hansen <luhansen@htwg-konstanz.de>
2018-01-01 18:35:29 +0100

    config

--- build.sbt
+++ build.sbt
@@ -10,8 +10,9 @@ libraryDependencies ++= {
   val scalaTestV = "3.0.0-M15"
   val scalaMockV = "3.2.2"
   val scalaFxV = "8.0.92-R10"
+  val configV = "1.3.1"
 
-  Seq(
+  Seq("com.typesafe" % "config" % configV,
     "org.scalatest" %% "scalatest" % scalaTestV % "test",
     "org.scalamock" %% "scalamock-scalatest-support" % scalaMockV % "test",
     "org.scalafx" %% "scalafx" % scalaFxV
--- src/main/scala/de/htwg/se/battleship/Battleship.scala
+++ src/main/scala/de/htwg/se/battleship/Battleship.scala
@@ -1,20 +1,23 @@
 package de.htwg.se.battleship
 
+import com.typesafe.config.ConfigFactory
 import de.htwg.se.battleship.controller.Controller
-import de.htwg.se.battleship.model._
 import de.htwg.se.battleship.view._
 
 object Battleship {
 
-  val fieldSizeDefault = 20
-  val fieldSizeEnvName = "fieldSize"
-
   def main(args: Array[String]): Unit = {
 
-    val fieldSize: Int = if (sys.env(fieldSizeEnvName).isEmpty) fieldSizeDefault else sys.env(fieldSizeEnvName).toInt
-
+    val (fieldSize, dummy) = getConfig
     val controller = Controller(fieldSize, new TuiView)
         controller.gameStart()
 
+  }
+
+  def getConfig = {
+    val config = ConfigFactory.load()
+    val fieldSize = config.getInt("battleship.fieldSize")
+    val dummy = config.getString("battleship.dummy")
+    (fieldSize, dummy)
   }
 }
