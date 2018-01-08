1a5c93d69e3f419a4c6706b7030011718e51bf27
Lukas Hansen <luhansen@htwg-konstanz.de>
2018-01-08 13:43:31 +0100

    formatting

--- src/main/scala/de/htwg/se/battleship/model/Message.scala
+++ src/main/scala/de/htwg/se/battleship/model/Message.scala
@@ -7,20 +7,16 @@ object Message {
   case class RegisterObserver() //registers observer and "responds" current state
   case class UnregisterObserver() //unregisters observer
 
-  //current phases of the game; what is the controller waiting for
-  sealed trait Phase
-
   case class PrintMessage(message: String) //ask to print/react to a message (e.g. "invalid placement")
 
   case class Update(state: Phase, activePlayer: Player, otherPlayer: Player) //update observers after action or registration
 
-  case object Init extends Phase
 
+  sealed trait Phase //current phases of the game; what is the controller waiting for
+  case object Init extends Phase // game not started yet
   case object PlaceShipTurn extends Phase //ask to place ship (coordinates, size and orientation)
   case object ShootTurn extends Phase //ask to shoot at coordinates
-  case object AnnounceWinner extends Phase
-
-  //announce winner
+  case object AnnounceWinner extends Phase //announce winner
 
   //actions of player
   case class PlaceShip(player: Player, startPoint: Point, shipSize: Int, orientation: Orientation) //place ship
